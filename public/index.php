d5a049ed154019e4c32fd20918ce05de4e6efbd2
itcocoa <3216730556@qq.com>
2015-11-23 14:35:23 +0800

    手动合并

--- public/index.php
+++ public/index.php
@@ -19,8 +19,5 @@ require 'init_autoloader.php';
 
 // Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
-<<<<<<< HEAD
 // Egit Added.
-=======
 // Github Added!!!
->>>>>>> branch 'master' of https://github.com/solody/xf.git
